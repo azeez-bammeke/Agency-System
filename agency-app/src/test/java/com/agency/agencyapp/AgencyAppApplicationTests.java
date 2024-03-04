0ef4fbe93f409f5ad64e54512fb7c7ab261544c0
abammeke <abammeke222@gmail.com>
2024-03-04 12:32:26 -0600

    [001] - initial commit: project created

---
+++ agency-app/src/test/java/com/agency/agencyapp/AgencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.agencyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
